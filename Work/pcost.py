debe0e4cfdbf4d1436cb6434732f75d9df39e0cb
sukumar <dadireddysukumar@gmail.com>
2020-06-02 12:56:55 +0530

    created function to calculate the total cost of all stocks

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(path):
+    data=open(path,'r')
+    headers=data.readline()
+    tc=0
+    for i in data:
+        tc=(int(i.split(',')[1])*float(i.split(',')[2]))+tc
+    return tc
+
+tc=portfolio_cost('Data/portfolio.csv')
+print(tc)
