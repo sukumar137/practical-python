b17f36719ac898dc0be7abdb407072b0a0893f96
sukumar <dadireddysukumar@gmail.com>
2020-06-04 21:14:16 +0530

    created a function called read_portfolio ,that creates list of tuples with stock info by reading if form a csv file

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+def read_portfolio(path):
+    import csv
+    portfolio=[]
+    with open(path,'r') as data:
+        rows=csv.reader(data)
+        headers=next(rows)
+        for stock in rows:
+            #s=stock.split(',')
+            #print(stock)
+
+            portfolio.append((stock[0],int(stock[1]),float(stock[2])))
+    return portfolio
+
+port=read_portfolio(r'Data\\portfolio.csv')
+print(port)
